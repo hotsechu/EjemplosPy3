1b1cb849271bb4719ace5769070e8bfc26ed1c0c
hotsechu <jose@hotsechu.es>
2020-11-08 19:58:55 +0100

    Primera codificación

--- acertarNumero.py
+++ acertarNumero.py
@@ -5,24 +5,43 @@ import sys
 import random
 
 
-def pedir_numero(texto):
-    numero = input(texto)
+def pedir_entero(texto):
+    try:
+        numero = int(input(texto))
+
+    except ValueError as e:
+        print("Error, no hay valor o no es numérico", file=sys.stderr)
+        print(e, file=sys.stderr)
+        sys.exit()
+
     return numero
 
 
-def pedir_numero_intervalo(texto, minimo, maximo):
+def pedir_entero_intervalo(texto, minimo, maximo):
     texto_llamada = texto + " entre " + str(minimo) + " y " + str(maximo) + " : "
-    numero = pedir_numero(texto_llamada)
+    numero = pedir_entero(texto_llamada)
     return numero
 
 
 def jugar():
-    MIN = 0
-    MAX = 100
+    minimo = 0
+    maximo = 100
+    valorCorrecto = random.randint(minimo, maximo)
+    acertado = False
+    intentos = 0
+    while not acertado:
+        intentos = intentos + 1
+        numero = pedir_entero_intervalo("Dime un número ", minimo, maximo)
+        if numero == valorCorrecto:
+            print("Enhorabuena, has acertado en " + str(intentos) + " intentos")
+            acertado = True
+        elif numero < valorCorrecto:
+            minimo = numero
+            print("Demasiado pequeño")
+        else:
+            maximo = numero
+            print("Demasiaso grande")
 
-    numero = pedir_numero_intervalo("Dime un número ", MIN, MAX)
-
-    print(numero)
     return
 
 
