0777f21e89b70176ce4fa70c27633709294c078c
hotsechu <jose@hotsechu.es>
2020-11-18 09:19:21 +0100

    Clase Punto: codificación inicial

---
+++ Punto.py
@@ -0,0 +1,51 @@
+class Punto:
+    """
+    Clase ejemplo para entender la declaración de clases en Python
+    """
+    def __init__(self, x, y, z):
+        """
+        Constructor de la clase. El primer parámetro
+        representa la instancia en curso. "self" no es una
+        palabra clave, podría usarse otro nombre, como "this"
+        o el que se prefiera.
+        """
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def mostrar(self):
+        print("Punto({}, {}, {})".format(self.x, self.y, self.z))
+
+    def __add__(self, other):
+        """
+        Operador suma (en el código será un "+")
+        :param other: Punto que se desea sumar al actual
+        :return: Suma de ambos puntos
+        """
+        return Punto(self.x + other.x,
+                     self.y + other.y,
+                     self.z + other.z)
+
+    def __sub__(self, other):
+        """
+        Operador resta (en el código será un "-")
+        :param other: Punto que se desea restar al actual
+        :return: Resta de ambos puntos
+        """
+        return Punto(self.x - other.x,
+                     self.y - other.y,
+                     self.z - other.z)
+
+
+# No es necesario niguna palabra clave para invocar la construcción
+# de un objeto, la propia declaración utiliza el método __init__
+px = Punto(1, -3, 4)
+px.mostrar()
+
+py = Punto(3, 3, 3)
+
+pz = px + py
+pz.mostrar()
+
+pz = px - py
+pz.mostrar()
